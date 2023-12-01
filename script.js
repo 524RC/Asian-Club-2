82214b4dda80ed9037e260ff797f7e5e2f9e236a
Ibrahium-Lashari <524Ilashari@frhsd.com>
2023-12-01 11:55:38 -0500

    java

--- script.js
+++ script.js
@@ -45,61 +45,65 @@ function person1() {
     document.getElementById('myImage')
       .src = "Mask group (2).png";
     document.getElementById('message')
-      .innerHTML = "";
+      .innerHTML = "Alayna Zayb";
+    document.getElementById('message1')
+      .innerHTML = "Hey Everyone! Im Alayna Zayb, your president this year for Asian Club. I am excited to host our cherished annual events as well as introduce new and exciting events to the community! I have been in Asian Club since sophomore year, and each year since I have always enjoyed the cultural celebrations and made unforgettable memories with my friends. My goal is to extend the same joy to everyone else attending club activities and events. As your president, I cant wait to make this 2023-2024 year the best one yet!
+      ";
   }
+}
   
   function person2() {
     document.getElementById('myImage')
       .src = "Mask group (3).png";
     document.getElementById('message')
-      .innerHTML = "";
+      .innerHTML = "Anika Bhatt";
   }
 
   function person3() {
     document.getElementById('myImage')
       .src = "Mask group (4).png";
     document.getElementById('message')
-      .innerHTML = "";
+      .innerHTML = "Ann Alex";
   }
 
   function person4() {
     document.getElementById('myImage')
       .src = "Mask group (5).png";
     document.getElementById('message')
-      .innerHTML = "";
+      .innerHTML = "Madelinh Weiner";
   }
 
   function person5() {
     document.getElementById('myImage')
       .src = "Mask group (6).png";
     document.getElementById('message')
-      .innerHTML = "";
+      .innerHTML = "Annika Bhatia";
   }
 
   function person6() {
     document.getElementById('myImage')
       .src = "Mask group (7).png";
     document.getElementById('message')
-      .innerHTML = "";
+      .innerHTML = "Alayna Lee";
   }
 
   function person7() {
     document.getElementById('myImage')
       .src = "Mask group (8).png";
     document.getElementById('message')
-      .innerHTML = "";
+      .innerHTML = "James Wang";
   }
 
   function person8() {
     document.getElementById('myImage')
       .src = "Mask group (9).png";
     document.getElementById('message')
-      .innerHTML = "";
+      .innerHTML = "Niru Gupta";
   }
 
   function person9() {
     document.getElementById('myImage')
       .src = "Mask group (10).png";
     document.getElementById('message')
-      .innerHTML = "";
+      .innerHTML = "Sydney Yuen";
   }
